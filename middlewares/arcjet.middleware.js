e1a46e70e92b5ee44251282a6fc88d2dc23ac598
YFRP01 <yfrp.dev@gmail.com>
2026-07-30 10:45:45 +0100

    update arcjet settings

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -4,7 +4,9 @@ import { isSpoofedBot } from "@arcjet/inspect";
 const arcjetMiddleware = async (req, res, next) => {
     try {
         const decision = await aj.protect(req, { requested: 1 });   
-
+        console.log("Arcjet decision:", decision.conclusion);
+        console.log("Arcjet decision:", decision.reason);
+        
         if (decision.isDenied()) {
             if (decision.reason.isRateLimit()) {
                 return res.status(429).json({ error: "Too Many Requests" });
@@ -14,20 +16,12 @@ const arcjetMiddleware = async (req, res, next) => {
             } 
             return res.status(403).json({ error: "Forbidden" });
         }
-        if (decision.ip.isHosting()) {
-            // Requests from hosting IPs are likely from bots, so they can usually be
-            // blocked. However, consider your use case - if this is an API endpoint
-            // then hosting IPs might be legitimate.
-            // https://docs.arcjet.com/blueprints/vpn-proxy-detection
-            return res.status(403).json({ error: "Forbidden" });
-        } 
-        if(decision.results.some(isSpoofedBot)) {
-            // Paid Arcjet accounts include additional verification checks using IP data.
-            // Verification isn't always possible, so we recommend checking the decision
-            // separately.
-            // https://docs.arcjet.com/bot-protection/reference#bot-verification
-            return res.status(403).json({ error: "No bots allowed" });
-        } 
+        // if (decision.ip.isHosting()) {
+        //     return res.status(403).json({ error: "Forbidden" });
+        // } 
+        // if(decision.results.some(isSpoofedBot)) {
+        //     return res.status(403).json({ error: "No bots allowed" });
+        // } 
         next()
     } catch (error) {
         console.log(`Arcjet Middleware Error: ${error}`);
